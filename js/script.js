1759da3d305e7dbbfc4504e812062973e95308d0
Sardor Akbarovich <sardor.cm.1287@gmail.com>
2023-06-02 08:05:40 +0500

    complate the homework

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let myName = "Mening ismim: "
+let mySurname = "familiyam: "
+let myAge = "yoshim: "
+let myEducation = "o'qish joyim: "
+let myHobby = "hobbiyim: "
+
+let name = "Sardor"
+let surname = "Odilov"
+let age = "16 da"
+let education = "10-maktab"
+let hobby = "Coding"
+
+console.log(myName + name + " , " + mySurname + surname + " , " + myAge + age + " , " + myEducation + education + " , " + myHobby + hobby)
+
